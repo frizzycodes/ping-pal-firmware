e4f64d05932907d97825345753d6ad0877b8372f
Adithya S Pai <234880574+frizzycodes@users.noreply.github.com>
2026-01-11 01:34:43 +0530

    Complete Wi-Fi bring-up with guarded event handling and state transitions

--- src/PingPalApp.cpp
+++ src/PingPalApp.cpp
@@ -1,5 +1,8 @@
 #include "hardware/Button.h"
 #include "PingPalApp.h"
+#include <WiFi.h>
+#include <WiFiType.h>
+#include <WiFiGeneric.h>
 #include <stdio.h>
 
 PingPalApp::PingPalApp()
@@ -61,8 +64,16 @@ void PingPalApp::onButtonShortPress()
 }
 
 // Wi-Fi events
-void PingPalApp::onWiFiConnected() {}
-void PingPalApp::onWiFiDisconnected() {}
+void PingPalApp::onWiFiConnected()
+{
+    stateMachine.transitionTo(State::ONLINE_PINGING);
+    onStateEntered(State::ONLINE_PINGING);
+}
+void PingPalApp::onWiFiDisconnected()
+{
+    stateMachine.transitionTo(State::WIFI_DISCONNECTED);
+    onStateEntered(State::WIFI_DISCONNECTED);
+}
 
 // Ping events
 void PingPalApp::onPingSuccess()
@@ -114,12 +125,14 @@ void PingPalApp::onStateEntered(State newState)
     switch (newState)
     {
     case State::BOOT:
+        startWiFiConnection();
         break;
     case State::SETUP_MODE:
         break;
     case State::CONNECTING_WIFI:
         break;
     case State::ONLINE_PINGING:
+        startPing();
         break;
     case State::ONLINE_PING_OK:
         break;
@@ -131,5 +144,36 @@ void PingPalApp::onStateEntered(State newState)
         break;
     }
 }
-void PingPalApp::startWiFiConnection() {}
+void PingPalApp::startWiFiConnection()
+{
+    static bool wifiEventRegistered = false;
+    if (!wifiEventRegistered)
+    {
+        WiFi.setHostname("ping-pal");
+        WiFi.mode(WIFI_STA);
+        WiFi.onEvent([this](arduino_event_id_t event, arduino_event_info_t info)
+                     {
+            switch (event)
+            {
+            case ARDUINO_EVENT_WIFI_STA_CONNECTED:
+                stateMachine.transitionTo(State::CONNECTING_WIFI);
+                onStateEntered(State::CONNECTING_WIFI);
+                break;
+            case ARDUINO_EVENT_WIFI_STA_GOT_IP:
+                onWiFiConnected();
+                break;
+            case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
+                onWiFiDisconnected();
+                break;    
+            default:
+                break;
+            } });
+        wifiEventRegistered = true;
+    }
+
+    // TODO: replace with stored credentials later
+    const char *ssid = "YOUR_WIFI";
+    const char *password = "YOUR_PASSWORD";
+    WiFi.begin(ssid, password);
+}
 void PingPalApp::startPing() {}
